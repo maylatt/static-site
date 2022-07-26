2c94e30241a0c346d1228cf7f2fb1d9f77621e73
Mayla Teixeira <maylateixeira2013@gmail.com>
2022-07-25 21:33:40 -0300

    feat: add pages html

---
+++ src/api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+PAGES_SUB_PATH = os.getenv("PAGES_SUB_PATH", "pages")
+
+directory_path = f"{os.path.dirname(__file__)}/{PAGES_SUB_PATH}"
+
+app.mount("/site", StaticFiles(directory=directory_path), name="static")
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/site/index.html")
